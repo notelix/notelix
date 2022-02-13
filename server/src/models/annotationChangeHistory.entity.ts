165a02bcfd1ee5a23231dfa36d17dde19f3696b3
Ke Wang <me@ke.wang>
2022-02-13 21:48:57 +0800

    add model AnnotationChangeHistory

---
+++ server/src/models/annotationChangeHistory.entity.ts
@@ -0,0 +1,42 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+const AnnotationChangeHistoryKindSave = 1;
+const AnnotationChangeHistoryKindDelete = 2;
+
+@Entity()
+@Index(['user'])
+export class AnnotationChangeHistory extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int' })
+  kind: number;
+
+  @Column({ type: 'int' })
+  annotationId: number;
+
+  @Column({ type: 'varchar', length: 64, unique: true })
+  uid: string;
+
+  @Column({ type: 'json' })
+  data: any;
+
+  @ManyToOne(() => User, (user) => user.annotations)
+  user: User;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+}
