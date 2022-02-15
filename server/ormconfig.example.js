81461a141103ce529a86aec1507216fd9ecb66fb
Ke Wang <me@ke.wang>
2022-02-15 15:30:40 +0800

    change DB to postgres

--- server/ormconfig.example.js
+++ server/ormconfig.example.js
@@ -1,8 +1,8 @@
 module.exports = {
-  type: 'mysql',
+  type: 'postgres',
   host: '127.0.0.1',
-  port: 3306,
-  username: 'root',
+  port: 18555,
+  username: 'postgres',
   password: '123456',
   database: 'notelix',
   entities: ['./dist/src/models/**/*.entity.js'],
