eed47616571999b1aaac12a80b5a77e099de7a40
Ke Wang <me@ke.wang>
2022-03-15 21:20:07 +0800

    copy licenses in build-extension.sh

--- chrome-extension/build-extension.sh
+++ chrome-extension/build-extension.sh
@@ -11,3 +11,6 @@ cp -r ./public $DIR_NAME
 cp extension-options* $DIR_NAME
 cp app* $DIR_NAME
 cp manifest.json $DIR_NAME
+mkdir -p $DIR_NAME/LICENSES
+cp LICENSES/* $DIR_NAME/LICENSES
+cp ../LICENSE $DIR_NAME/LICENSES/notelix.LICENSE
