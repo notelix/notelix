3ebbf6b043c2aa4779230fd941c458f4ae85e651
Paulo Henrique Eli <@paulo.eli>
2023-07-20 19:37:10 -0300

    Update build-extension.sh to include images; update manifest.json

--- chrome-extension/build-extension.sh
+++ chrome-extension/build-extension.sh
@@ -8,6 +8,7 @@ npm run build:popup
 npm run build:app
 cp -r ./dist $DIR_NAME
 cp -r ./public $DIR_NAME
+cp -r ./images $DIR_NAME
 cp extension-options* $DIR_NAME
 cp app* $DIR_NAME
 cp manifest.json $DIR_NAME
