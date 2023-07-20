607c32d5611b29ccfef901a691e2a57e7c94a0a2
Paulo Henrique Eli <@paulo.eli>
2023-07-20 18:48:08 -0300

    Fixing path of index file in poup component

--- chrome-extension/src/popup/App.jsx
+++ chrome-extension/src/popup/App.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 
-import { HashRouter, Route, Switch } from "react-router-dom";
-import { LogIn } from "./pages/LogIn";
-import { SetServer } from "./pages/SetServer";
-import { SignUp } from "./pages/SignUp";
-import { UserInfo } from "./pages/UserInfo";
-import { Index } from "./pages";
-import { ChangePassword } from "./pages/ChangePassword";
+import {HashRouter, Route, Switch} from "react-router-dom";
+import {LogIn} from "./pages/LogIn";
+import {SetServer} from "./pages/SetServer";
+import {SignUp} from "./pages/SignUp";
+import {UserInfo} from "./pages/UserInfo";
+import {Index} from "./pages/Index";
+import {ChangePassword} from "./pages/ChangePassword";
 
 const App = () => {
-  return (
-    <HashRouter>
-      <Switch>
-        <Route path="/set-server">
-          <SetServer />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/user-info">
-          <UserInfo />
-        </Route>
-        <Route path="/login">
-          <LogIn />
-        </Route>
-        <Route path="/change-password">
-          <ChangePassword />
-        </Route>
-        <Route path="/">
-          <Index />
-        </Route>
-      </Switch>
-    </HashRouter>
-  );
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/set-server">
+                    <SetServer/>
+                </Route>
+                <Route path="/signup">
+                    <SignUp/>
+                </Route>
+                <Route path="/user-info">
+                    <UserInfo/>
+                </Route>
+                <Route path="/login">
+                    <LogIn/>
+                </Route>
+                <Route path="/change-password">
+                    <ChangePassword/>
+                </Route>
+                <Route path="/">
+                    <Index/>
+                </Route>
+            </Switch>
+        </HashRouter>
+    );
 };
 
 export default App;
