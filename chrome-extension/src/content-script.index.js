6bb8c18360b47b141eda399a0342844904543298
Paulo Henrique Eli <@paulo.eli>
2023-07-30 19:22:29 -0300

    Remove some configurations to use embedded and with encryption

--- chrome-extension/src/content-script.index.js
+++ chrome-extension/src/content-script.index.js
@@ -1,51 +1,38 @@
 import "./styles.less";
 import "babel-polyfill";
-import {prepareDomElements} from "./dom";
-import {whenUrlChanges} from "./utils/whenUrlChanges";
-import {marker} from "./marker";
-import {loadAllAnnotationsData} from "./service";
-import {registerHotkeys} from "./hotkeys";
-import {reactToSelection} from "./selection";
-import {registerChromeRuntimeMessageListeners} from "./chrome";
+import { prepareDomElements } from "./dom";
+import { whenUrlChanges } from "./utils/whenUrlChanges";
+import { marker } from "./marker";
+import { loadAllAnnotationsData } from "./service";
+import { registerHotkeys } from "./hotkeys";
+import { reactToSelection } from "./selection";
+import { registerChromeRuntimeMessageListeners } from "./chrome";
 
 setTimeout(() => {
 
-    if (document.body.className.indexOf("notelix-initialized") >= 0) {
-        return;
-    } else {
-        document.body.className = document.body.className + " notelix-initialized";
-    }
+  if (document.body.className.indexOf("notelix-initialized") >= 0) {
+    return;
+  } else {
+    document.body.className = document.body.className + " notelix-initialized";
+  }
 
-    if (
-        !(
-            window.NotelixEmbeddedConfig &&
-            window.NotelixEmbeddedConfig.disableLoadAllAnnotationsDataWhenUrlChanges
-        )
-    ) {
-        whenUrlChanges(() => {
-            setTimeout(() => {
-                loadAllAnnotationsData();
-            });
-        });
-    }
+  whenUrlChanges(() => {
+    setTimeout(() => {
+      loadAllAnnotationsData();
+    });
+  });
 
-    registerChromeRuntimeMessageListeners();
+  registerChromeRuntimeMessageListeners();
 
-    prepareDomElements();
+  prepareDomElements();
 
-    marker.addEventListeners();
+  marker.addEventListeners();
 
-    registerHotkeys();
+  registerHotkeys();
 
-    reactToSelection();
+  reactToSelection();
 
-    // doTrySetAgentSyncParamsLoop();
-
-    // window.NotelixAPI = {
-    //     loadAllAnnotationsData,
-    // };
-
-    if (window.NotelixEmbeddedConfig && window.NotelixEmbeddedConfig.onReady) {
-        window.NotelixEmbeddedConfig.onReady();
-    }
+  if (window.NotelixEmbeddedConfig && window.NotelixEmbeddedConfig.onReady) {
+    window.NotelixEmbeddedConfig.onReady();
+  }
 });
